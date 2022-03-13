9ded8a99301a904cfdd4c9cd27f19ec011a0697a
takeokikkuchi <96605483+takeokikkuchi@users.noreply.github.com>
2022-03-13 16:18:46 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,36 +3,27 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import schedule
+import time
+import snscrape.modules.twitter as sntwitter
 
-"""
-# Welcome to Streamlit!
+tweets_list1 = []
+# 実行job関数
+def job():
+    tweets_list1 = []
+    # Using TwitterSearchScraper to scrape data and append tweets to list
+    for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:jack').get_items()):
+        if i>100:
+            break
+        tweets_list1.append([tweet.date, tweet.id, tweet.content, tweet.username])
+    
+    # Creating a dataframe from the tweets list above 
+    tweets_df1 = pd.DataFrame(tweets_list1, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
+    st.write(tweets_df1)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+schedule.every(2).minutes.do(job)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+#while True:
+for i in range(1,10):
+    schedule.run_pending()
+    time.sleep(1)
