a7a655c209eb6ddf2e8b569c443dba08fa6a84a9
takeokikkuchi <96605483+takeokikkuchi@users.noreply.github.com>
2022-04-11 20:36:01 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,18 +14,70 @@ st.write("hello")
 # streamlit_app.py
 
 import streamlit as st
+import pandas as pd
+import sqlite3 
+import hashlib
 
-# Everything is accessible via the st.secrets dict:
+conn = sqlite3.connect('database.db')
+c = conn.cursor()
 
-st.write("DB username:", st.secrets["db_username"])
-st.write("DB password:", st.secrets["db_password"])
-st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+import hashlib
+def make_hashes(password):
+	return hashlib.sha256(str.encode(password)).hexdigest()
 
-# And the root-level secrets are also accessible as environment variables:
+def check_hashes(password,hashed_text):
+	if make_hashes(password) == hashed_text:
+		return hashed_text
+	return False
 
-import os
+def create_user():
+	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
 
-st.write(
-    "Has environment variables been set:",
-    os.environ["db_username"] == st.secrets["db_username"],
-)
+def add_user(username,password):
+	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
+	conn.commit()
+
+def login_user(username,password):
+	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
+	data = c.fetchall()
+	return data
+
+def main():
+
+	st.title("ログイン機能テスト")
+
+	menu = ["ホーム","ログイン","サインアップ"]
+	choice = st.sidebar.selectbox("メニュー",menu)
+
+	if choice == "ホーム":
+		st.subheader("ホーム画面です")
+
+	elif choice == "ログイン":
+		st.subheader("ログイン画面です")
+
+		username = st.sidebar.text_input("ユーザー名を入力してください")
+		password = st.sidebar.text_input("パスワードを入力してください",type='password')
+		if st.sidebar.checkbox("ログイン"):
+			create_user()
+			hashed_pswd = make_hashes(password)
+
+			result = login_user(username,check_hashes(password,hashed_pswd))
+			if result:
+
+				st.success("{}さんでログインしました".format(username))
+
+			else:
+				st.warning("ユーザー名かパスワードが間違っています")
+
+	elif choice == "サインアップ":
+		st.subheader("新しいアカウントを作成します")
+		new_user = st.text_input("ユーザー名を入力してください")
+		new_password = st.text_input("パスワードを入力してください",type='password')
+
+		if st.button("サインアップ"):
+			create_user()
+			add_user(new_user,make_hashes(new_password))
+			st.success("アカウントの作成に成功しました")
+			st.info("ログイン画面からログインしてください")
+if __name__ == '__main__':
+	main()
